67ae4a14376d64bf137d9c4b928dd3927ce012c5
aasmyb <m.a.elkholaey@gmail.com>
2022-09-25 15:34:45 +0200

    Add css, postcss and sass support

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,5 @@
 // https://forkify-api.herokuapp.com/v2
+import '../styles/main.scss';
 import * as model from './model';
 import recipeView from './views/recipeView';
 import searchView from './views/searchView';
